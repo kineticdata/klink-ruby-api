63f85dd707c835886f315acf275dce57baa437da
jdsundberg <jdsundberg@b104f756-63e8-4b76-83b0-355f72fca733>
2010-03-11 19:05:17 +0000

    Cleanup of old code
    git-svn-id: https://kineticdata.svn.cvsdude.com/klinkRubyApi/trunk@3 b104f756-63e8-4b76-83b0-355f72fca733

--- lib/kinetic_link.rb
+++
@@ -1,5 +0,0 @@
-# Include the current lib directory in the Ruby load path
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-require 'kinetic/link'
